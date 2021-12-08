91b45dbde091f01b6059175f44137f35ce185d72
gabisanto <gabrielasantoriello@gmail.com>
2021-12-08 16:18:53 -0300

    Subiendo el ejercicio

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require ("path")
+const express = require ("express")
+const app = express ()
+
+app.set ("port", process.env.PORT || 3001)
+
+app.listen (app.get("port"), () => {
+    console.log ("Servidor OK");
+});
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')))
+
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
+
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')))
+
+app.use(express.static(path.resolve(__dirname,'./public')))
