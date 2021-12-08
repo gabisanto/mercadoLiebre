7afae7b3c35e589b7a7dc3f7578fffdc15788377
gabisanto <gabrielasantoriello@gmail.com>
2021-12-08 16:41:20 -0300

    app cambio

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.listen (app.get("port"), () => {
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')))
 
+app.get('/home',(req,res) => res.sendFile(path.resolve(__dirname,'./views/home.html')))
+
 app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
 
 app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'./views/login.html')))
